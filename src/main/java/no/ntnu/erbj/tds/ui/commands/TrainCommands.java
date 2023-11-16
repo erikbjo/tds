8dba1eff8c09fd00c77bb81c7ffb875ab1dfad83
Erik Bjørnsen <bjornsen.erik@gmail.com>
2023-11-16 10:49:46 +0100

    integrated printer class, updated versions

--- src/main/java/no/ntnu/erbj/tds/ui/commands/TrainCommands.java
+++ src/main/java/no/ntnu/erbj/tds/ui/commands/TrainCommands.java
@@ -4,10 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import no.ntnu.erbj.tds.dao.TrainDao;
 import no.ntnu.erbj.tds.model.Train;
-import no.ntnu.erbj.tds.ui.utilities.AnsiColors;
-import no.ntnu.erbj.tds.ui.utilities.Colorize;
-import no.ntnu.erbj.tds.ui.utilities.SortUtility;
-import no.ntnu.erbj.tds.ui.utilities.TdsLogger;
+import no.ntnu.erbj.tds.ui.utilities.*;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,17 +13,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * Commands for manipulating train objects.
  *
  * @author Erik
- * @version 1.1
+ * @version 2.0
  */
 @ShellComponent
 @EnableTransactionManagement
 public class TrainCommands {
-  private static final String TABLE_FORMAT = "| %-15s | %-10s | %-40s |";
-  // Above and below are static to follow SonarLint's recommendation
-  private static final String DIVIDER =
-      "+-----------------+------------+------------------------------------------+";
-  private final String blueFormat = Colorize.colorizeText(AnsiColors.BLUE, TABLE_FORMAT);
-  private final String blueDivider = Colorize.colorizeText(AnsiColors.BLUE, DIVIDER);
   private final TrainDao trainDao;
 
   /**
@@ -38,13 +29,6 @@ public class TrainCommands {
     this.trainDao = trainDao;
   }
 
-  private void printTableHeader() {
-    TdsLogger logger = TdsLogger.getInstance();
-    logger.info(blueDivider);
-    logger.info(String.format(blueFormat, "Train number", "Seats", "Wagons"));
-    logger.info(blueDivider);
-  }
-
   /** List all trains. */
   @ShellMethod(value = "List all trains.", key = "train list")
   public void listTrains() {
@@ -54,23 +38,10 @@ public class TrainCommands {
   /** Stylised table of all trains, sorted by number of wagons. */
   @ShellMethod(value = "List all trains in a stylised table.", key = "train table")
   public void listTrainTable() {
-    TdsLogger logger = TdsLogger.getInstance();
-
     List<Train> trains =
         SortUtility.sortBy(trainDao.getAll(), Comparator.comparing(Train::getNumberOfWagons));
 
-    printTableHeader();
-
-    for (Train train : trains) {
-      logger.info(
-          String.format(
-              TABLE_FORMAT,
-              train.getTrainNumber(),
-              train.getNumberOfSeats(),
-              train.getNumberOfWagons()));
-    }
-
-    logger.info(DIVIDER);
+    Printer.printTrainsInTableFormat(trains);
   }
 
   /** Stylised table of all un trains, sorted by number of wagons. */
@@ -78,23 +49,10 @@ public class TrainCommands {
       value = "List all unoccupied trains in a stylised table.",
       key = "train unoccupied table")
   public void listUnoccupiedTrainsTable() {
-    TdsLogger logger = TdsLogger.getInstance();
-
     List<Train> trains =
         SortUtility.sortBy(
             trainDao.getAllUnoccupiedTrains(), Comparator.comparing(Train::getNumberOfWagons));
 
-    printTableHeader();
-
-    for (Train train : trains) {
-      logger.info(
-          String.format(
-              TABLE_FORMAT,
-              train.getTrainNumber(),
-              train.getNumberOfSeats(),
-              train.getNumberOfWagons()));
-    }
-
-    logger.info(DIVIDER);
+    Printer.printTrainsInTableFormat(trains);
   }
 }
--- src/main/java/no/ntnu/erbj/tds/ui/utilities/Printer.java
+++ src/main/java/no/ntnu/erbj/tds/ui/utilities/Printer.java
@@ -2,18 +2,15 @@ package no.ntnu.erbj.tds.ui.utilities;
 
 import java.util.List;
 import no.ntnu.erbj.tds.model.Departure;
+import no.ntnu.erbj.tds.model.Train;
 
 /**
  * A class that provides printing functionality. <br>
- * Contains the following methods:
- *
- * <ul>
- *   <li>{@link #printInvalidInput(String)}
- * </ul>
- *
+ * Contains methods for printing messages to the console, as well as methods for printing lists of
+ * objects in a stylized table format. <br>
  * Use this rather than raw usage of TdsLogger, as this class is more specific.
  *
- * @version 1.1
+ * @version 2.0
  * @author erik
  */
 public class Printer {
@@ -29,6 +26,15 @@ public class Printer {
       Colorize.colorizeText(AnsiColors.BLUE, DEPARTURE_TABLE_FORMAT);
   private static final String BLUE_DEPARTURE_DIVIDER =
       Colorize.colorizeText(AnsiColors.BLUE, DEPARTURE_TABLE_DIVIDER);
+
+  private static final String TRAIN_TABLE_FORMAT = "| %-15s | %-10s | %-40s |";
+  // Above and below are static to follow SonarLint's recommendation
+  private static final String TRAIN_TABLE_DIVIDER =
+      "+-----------------+------------+------------------------------------------+";
+  private static final String BLUE_TRAIN_FORMAT =
+      Colorize.colorizeText(AnsiColors.BLUE, TRAIN_TABLE_FORMAT);
+  private static final String BLUE_TRAIN_DIVIDER =
+      Colorize.colorizeText(AnsiColors.BLUE, TRAIN_TABLE_DIVIDER);
 
   /**
    * Private constructor to prevent instantiation.
@@ -69,8 +75,8 @@ public class Printer {
   }
 
   /** Prints a message to the console asking the user to enter a departure delay. */
-  public static void printEnterDepartureDelay() {
-    logger.info("Enter departure delay (HH:mm): ");
+  public static void printEnterDelay() {
+    logger.info("Enter delay (HH:mm): ");
   }
 
   /** Prints a message to the console asking the user to enter a train number. */
@@ -123,6 +129,11 @@ public class Printer {
     logger.info(BLUE_DEPARTURE_DIVIDER);
   }
 
+  /** Prints the table divider for departure table. */
+  public static void printDepartureTableDivider() {
+    logger.info(DEPARTURE_TABLE_DIVIDER);
+  }
+
   /**
    * Prints a list of departures in a stylized table.
    *
@@ -140,6 +151,70 @@ public class Printer {
               departure.getTrain().getTrainNumber(),
               departure.getDestination()));
     }
-    printDepartureTableHeader();
+    printDepartureTableDivider();
+  }
+
+  /** Prints a message to the console informing the user that there are no departures in the db. */
+  public static void printNoDeparturesFound() {
+    logger.info("No departures found. Please create a departure first.");
+  }
+
+  /**
+   * Prints a message to the console informing the user that there are no departures with the given
+   * destination.
+   */
+  public static void printNoDeparturesWithDestination() {
+    logger.info("No departures found with destination.");
+  }
+
+  /**
+   * Prints a message to the console informing the user that there are no departures with the given
+   * train number.
+   */
+  public static void printNoDeparturesWithTrainNumber() {
+    logger.info("No departures found with train number.");
+  }
+
+  /**
+   * Prints a message to the console asking the user to enter a train number of a departure to set
+   * delay.
+   */
+  public static void printEnterTrainNumberForDelay() {
+    logger.info("Enter the train number of the departure you want to set the delay of: ");
+  }
+
+  /** Prints a message to the console informing the user that the delay was set successfully. */
+  public static void printDelaySetSuccessfully() {
+    logger.info(Colorize.colorizeText(AnsiColors.GREEN, "Delay set."));
+  }
+
+  /** Prints the table header for train table. */
+  public static void printTrainTableHeader() {
+    logger.info(BLUE_TRAIN_DIVIDER);
+    logger.info(String.format(BLUE_TRAIN_FORMAT, "Train number", "Seats", "Wagons"));
+    logger.info(BLUE_TRAIN_DIVIDER);
+  }
+
+  /** Prints the table divider for train table. */
+  public static void printTrainTableDivider() {
+    logger.info(TRAIN_TABLE_DIVIDER);
+  }
+
+  /**
+   * Prints a list of trains in a stylized table.
+   *
+   * @param trains the list of trains to print
+   */
+  public static void printTrainsInTableFormat(List<Train> trains) {
+    printTrainTableHeader();
+    for (Train train : trains) {
+      logger.info(
+          String.format(
+              TRAIN_TABLE_FORMAT,
+              train.getTrainNumber(),
+              train.getNumberOfSeats(),
+              train.getNumberOfWagons()));
+    }
+    printTrainTableDivider();
   }
 }
